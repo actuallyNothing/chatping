661e98f281435c2386781b4ebfc1db4a0c3d72d2
actuallyNothing <dializ144@gmail.com>
2020-10-09 18:48:16 -0300

    Fixed typos

--- chatping/lua/autorun/client/chatping.lua
+++ chatping/lua/autorun/client/chatping.lua
@@ -2,7 +2,7 @@ CreateClientConVar("chatping_enable", "1", true, false, "Enable chatpinging: pla
 CreateClientConVar("chatping_loopback", "0", true, false, "Allows the player to chatping themselves", 0, 1)
 CreateClientConVar("chatping_sound", "default", true, false, "Defines a custom sound for chatpinging. Set to 'default' for default addon sound. Set to 'custom' to play a custom sound (must be named 'chatping_sound.wav')")
 CreateClientConVar("chatping_alphanumeric", "0", true, false, "Defines whether the addon should ignore non-alphanumeric characters. This can help a bit with names that contain weird characters or that are not usually autocompleted, but it is not perfect and can cause issues with some characters. If set to 0, a perfect match must be achieved to play the sound (this can be done by autocompleting nicknames with the TAB key).", 0, 1)
-CreateClientConVar("chatping_casesentitive", "0", true, false, "Defines whether the addon is case-sensitive or not. Disabled by default, enable to only chatping whenever a perfect case-sensitive match is achieved.", 0, 1)
+CreateClientConVar("chatping_casesensitive", "0", true, false, "Defines whether the addon is case-sensitive or not. Disabled by default, enable to only chatping whenever a perfect case-sensitive match is achieved.", 0, 1)
 
 function chatpinging(ply, text)
 	-- Close up the function if the addon is disabled or if the player tries to ping themselves without loopback enabled
@@ -23,7 +23,7 @@ function chatpinging(ply, text)
 		return
 	end
 
-	-- Store player's nick as a variable
+	-- Store player's nick as a local variable
 	local nick = LocalPlayer():Nick()
 
 	-- Non-alphanumeric text handling
@@ -32,11 +32,11 @@ function chatpinging(ply, text)
 	end
 
 	-- Non case-sensitive text handling
-	if GetConVar('chatping_casesentitive'):GetBool() == false then
+	if GetConVar('chatping_casesensitive'):GetBool() == false then
 		nick = nick:lower()
 		text = text:lower()
 	end
-	
+
 	-- Try and find the nick within the message
 	-- If it is found, play sound
 	if string.find(text, nick) then
