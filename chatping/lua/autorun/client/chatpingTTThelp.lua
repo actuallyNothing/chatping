30ab346ede8ca12cd38712a156be2f0534d82897
actuallyNothing <dializ144@gmail.com>
2020-10-11 22:36:49 -0300

    Made design more consistent with the TTT helpmenu

--- chatping/lua/autorun/client/chatpingTTThelp.lua
+++ chatping/lua/autorun/client/chatpingTTThelp.lua
@@ -1,47 +1,53 @@
 function addPingTab(dtabs)
-	-- Run only in TTT
-	if engine.ActiveGamemode() != 'terrortown' then
-		return
-	end
 
 	-- Create initial panel and list
-	local chatpingPanel = vgui.Create("DPanel")
-	local pingSettings = vgui.Create("DPanelList", chatpingPanel)
+  	local padding = dtabs:GetPadding()
+  	local margin = 15 / 2
+  	local w, h = 630 / 2, 470 / 2
+  	local bw, bh = 50 / 2, 25 / 2
+
+	local pingSettings = vgui.Create("DPanelList", dtabs)
 	pingSettings:StretchToParent(0,0,padding,0)
 	pingSettings:EnableVerticalScrollbar(true)
 	pingSettings:SetPadding(10)
 	pingSettings:SetSpacing(10)
-	pingSettings:SetSize(590, 320)
 
 	-- Create a form
 	local pingUI = vgui.Create("DForm", pingSettings)
 	pingUI:SetName("Chatping settings")
-	pingUI:SetSize(590, 320)
+	pingUI:SetPos(8.8, 8.8)
+	pingUI:SetSize(w * 1.8, h - margin * 3 - bh)
 
 	-- Declare an empty variable, reusing it for each setting and item
-	-- Each setting is pretty self-explanatory, I'd say
 	local setting = nil
 	setting = pingUI:CheckBox("Enable Chatping", "chatping_enable")
 	setting:SetTooltip("Enables chatpinging.")
 
-	setting = pingUI:CheckBox("Enable Loopback", "chatping_loopback")
-	setting:SetTooltip("Allows you to chatping yourself.")
-
-	setting = pingUI:CheckBox("Alphanumerical matching", "chatping_alphanumeric")
-	setting:SetTooltip("Should the addon try and delete any symbols from the message before matching with your name?")
-
 	setting = pingUI:CheckBox("Case-sensitive matching", "chatping_casesensitive")
 	setting:SetTooltip("Should the addon care about uppercase letters?")
 
-	setting = pingUI:ComboBox("Chatping sound", "chatping_sound")
+	setting = pingUI:ComboBox("Chatping sound:", "chatping_sound")
 	setting:AddChoice("Default sound", 'default')
 	setting:AddChoice("Custom sound", 'custom')
 
 	setting = pingUI:Help("For a custom sound to work, it must be placed inside the /garrysmod/ folder, with a .wav format and the filename 'chatping_sound.wav'!")
-	setting:SetColor(Color(255,0,0))
+	setting:SetColor(Color(0,0,0))
 
-	// Add final tab to TTT settings
-	dtabs:AddSheet("Chatping", chatpingPanel, "icon16/comments.png")
+	aliasEnable = pingUI:CheckBox("Enable alias", "chatping_alias_enable")
+	setting:SetPos(5,20)
+	setting:SetSize(100,20)
+
+	aliasSet = pingUI:TextEntry("Alias:", "chatping_alias")
+	setting = pingUI:Help("An alias is another nickname to which the addon will react upon. If enabled, you will be chatpinged whenever your nickname or alias are typed in chat.")
+	setting:SetColor(Color(0,0,0))
+
+	-- Add final tab to TTT settings
+	dtabs:AddSheet("Chatping", pingSettings, "icon16/comments.png")
 end
 
-hook.Add("TTTSettingsTabs", "pingingpanel", addPingTab)
+-- Run only in TTT
+if engine.ActiveGamemode() != 'terrortown' then
+	return
+else
+	hook.Add("TTTSettingsTabs", "pingingpanel", addPingTab)
+end
