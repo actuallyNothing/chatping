0c0fbf75e0500cdbb66f47e91aa107cc888ce370
actuallyNothing <dializ144@gmail.com>
2020-11-04 07:56:26 -0300

    Autorun file initial creation

---
+++ chatping/lua/chatping/autorun/chatping_autorun.lua
@@ -0,0 +1,14 @@
+--[[
+
+	Chatping v1.1.0 
+	Made by actuallyNothing for the Trouble in Terrorist Town gamemode
+	https://steamcommunity.com/id/actuallyNothing/
+	https://github.com/actuallyNothing/chatping/
+
+--]]
+
+Chatping = Chatping or {}
+
+if CLIENT then
+	include("chatping/client/cl_init.lua")
+end
